37f6034a885499d6545a6193159aeb82f0891f39
Anooraag Basu <anooraagb@gmail.com>
2025-02-06 15:25:07 +0000

    Fixed errors in the RShiny App code

--- healthdashboardshinyapp.R
+++ healthdashboardshinyapp.R
@@ -1,12 +1,61 @@
-# Run the application
-shinyApp(ui = ui, server = server)
-
+# Load necessary libraries
 library(shiny)
 library(DBI)
 library(RSQLite)
 library(dplyr)
 library(ggplot2)
 library(shinydashboard)
+
+# Define Server Logic
+server <- function(input, output, session) {
+  con <- dbConnect(RSQLite::SQLite(), "healthdashboard.db")
+  
+  fetch_data <- function(query) {
+    dbGetQuery(con, query)
+  }
+  
+  # Activity Data
+  output$activity_plot <- renderPlot({
+    req(input$activity_update)
+    data <- fetch_data(paste("SELECT ActivityDate,", input$activity_metric, "FROM dailyActivity_merged",
+                             "WHERE ActivityDate BETWEEN '", input$activity_date[1], "' AND '", input$activity_date[2], "'"))
+    ggplot(data, aes(x = ActivityDate, y = get(input$activity_metric))) + geom_line() + theme_minimal()
+  })
+  
+  # Heart Rate Data
+  output$hr_plot <- renderPlot({
+    req(input$hr_update)
+    data <- fetch_data(paste("SELECT Time, HeartRate FROM heartrate_seconds_merged",
+                             "WHERE Time BETWEEN '", input$hr_date[1], "' AND '", input$hr_date[2], "'"))
+    ggplot(data, aes(x = Time, y = HeartRate)) + geom_line() + theme_minimal()
+  })
+  
+  # Calories Data
+  output$cal_plot <- renderPlot({
+    req(input$cal_update)
+    data <- fetch_data(paste("SELECT ActivityDate, Calories FROM dailyCalories_merged",
+                             "WHERE ActivityDate BETWEEN '", input$cal_date[1], "' AND '", input$cal_date[2], "'"))
+    ggplot(data, aes(x = ActivityDate, y = Calories)) + geom_line() + theme_minimal()
+  })
+  
+  # Sleep Data
+  output$sleep_plot <- renderPlot({
+    req(input$sleep_update)
+    data <- fetch_data(paste("SELECT SleepDay, TotalMinutesAsleep FROM sleepDay_merged",
+                             "WHERE SleepDay BETWEEN '", input$sleep_date[1], "' AND '", input$sleep_date[2], "'"))
+    ggplot(data, aes(x = SleepDay, y = TotalMinutesAsleep)) + geom_line() + theme_minimal()
+  })
+  
+  # Weight Data
+  output$weight_plot <- renderPlot({
+    req(input$weight_update)
+    data <- fetch_data(paste("SELECT Date, WeightKg FROM weightLogInfo_merged",
+                             "WHERE Date BETWEEN '", input$weight_date[1], "' AND '", input$weight_date[2], "'"))
+    ggplot(data, aes(x = Date, y = WeightKg)) + geom_line() + theme_minimal()
+  })
+  
+  onStop(function() { dbDisconnect(con) })
+}
 
 # Define UI
 ui <- dashboardPage(
@@ -27,7 +76,7 @@ ui <- dashboardPage(
               fluidRow(
                 box(title = "Filters", status = "primary", solidHeader = TRUE, 
                     selectInput("activity_metric", "Select Activity:",
-                                choices = c("Total Steps", "Total Distance", "Very Active Minutes")),
+                                choices = c("TotalSteps", "TotalDistance", "VeryActiveMinutes")),
                     dateRangeInput("activity_date", "Date Range:",
                                    start = "2016-01-01", end = Sys.Date()),
                     actionButton("activity_update", "Update View")
@@ -88,56 +137,5 @@ ui <- dashboardPage(
   )
 )
 
-# Define Server Logic
-server <- function(input, output, session) {
-  con <- dbConnect(RSQLite::SQLite(), "healthdashboard.db")
-  
-  fetch_data <- function(query) {
-    dbGetQuery(con, query)
-  }
-  
-  # Activity Data
-  output$activity_plot <- renderPlot({
-    req(input$activity_update)
-    data <- fetch_data(paste("SELECT ActivityDate,", input$activity_metric, "FROM dailyActivity_merged",
-                             "WHERE ActivityDate BETWEEN '", input$activity_date[1], "' AND '", input$activity_date[2], "'"))
-    ggplot(data, aes(x = ActivityDate, y = get(input$activity_metric))) + geom_line() + theme_minimal()
-  })
-  
-  # Heart Rate Data
-  output$hr_plot <- renderPlot({
-    req(input$hr_update)
-    data <- fetch_data(paste("SELECT Time, HeartRate FROM heartrate_seconds_merged",
-                             "WHERE Time BETWEEN '", input$hr_date[1], "' AND '", input$hr_date[2], "'"))
-    ggplot(data, aes(x = Time, y = HeartRate)) + geom_line() + theme_minimal()
-  })
-  
-  # Calories Data
-  output$cal_plot <- renderPlot({
-    req(input$cal_update)
-    data <- fetch_data(paste("SELECT ActivityDate, Calories FROM dailyCalories_merged",
-                             "WHERE ActivityDate BETWEEN '", input$cal_date[1], "' AND '", input$cal_date[2], "'"))
-    ggplot(data, aes(x = ActivityDate, y = Calories)) + geom_line() + theme_minimal()
-  })
-  
-  # Sleep Data
-  output$sleep_plot <- renderPlot({
-    req(input$sleep_update)
-    data <- fetch_data(paste("SELECT SleepDay, TotalMinutesAsleep FROM sleepDay_merged",
-                             "WHERE SleepDay BETWEEN '", input$sleep_date[1], "' AND '", input$sleep_date[2], "'"))
-    ggplot(data, aes(x = SleepDay, y = TotalMinutesAsleep)) + geom_line() + theme_minimal()
-  })
-  
-  # Weight Data
-  output$weight_plot <- renderPlot({
-    req(input$weight_update)
-    data <- fetch_data(paste("SELECT Date, WeightKg FROM weightLogInfo_merged",
-                             "WHERE Date BETWEEN '", input$weight_date[1], "' AND '", input$weight_date[2], "'"))
-    ggplot(data, aes(x = Date, y = WeightKg)) + geom_line() + theme_minimal()
-  })
-  
-  onStop(function() { dbDisconnect(con) })
-}
-
 # Run App
-shinyApp(ui, server)
+shinyApp(ui = ui, server = server)
