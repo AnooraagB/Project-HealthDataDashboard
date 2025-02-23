722e27a9c5d0e02e8d61fd008fadb362e35339a5
Anooraag Basu <anooraagb@gmail.com>
2025-02-23 12:45:52 +0000

    Updated Rscript.R

--- healthdashboardRscript.R
+++ healthdashboardRscript.R
@@ -1,35 +1,18 @@
-# Install and load required packages
-install.packages(c("httpuv", "fs", "sass", "bslib", "bit", "readr", "shinydashboard", "RSQLite", "DBI", "dplyr"), type = "binary")
-library(httpuv)
-library(fs)
-library(sass)
-library(bslib)
-library(bit)
 library(readr)
-library(shinydashboard)
 library(RSQLite)
 library(DBI)
-library(dplyr)
 
-# Define the directory and get the list of CSV files
-file_list <- list.files(path = "/Users/anooraagbasu/Documents/HealthDashboard/data/Fitbit_Data", pattern = "*.csv", full.names = TRUE)
+data_file <- "fit.txt"
 
-# Read each CSV file and assign to variable dynamically
-for (file in file_list) {
-  file_name <- gsub(".csv", "", basename(file))
-  assign(file_name, read_csv(file))
-}
+data <- read_delim(data_file, delim = "\t")
 
-# Create SQLite connection and write data to database
+# Create SQLite connection
 con <- dbConnect(RSQLite::SQLite(), "healthdashboard.db")
-for (file in file_list) {
-  file_name <- gsub(".csv", "", basename(file))
-  df <- read_csv(file)
-  dbWriteTable(con, file_name, df, overwrite = TRUE)
-}
 
-# Query and print the first 10 rows of a table
-query_result <- dbGetQuery(con, "SELECT * FROM dailyActivity_merged LIMIT 10")
+dbWriteTable(con, "fit_data", data, overwrite = TRUE)
+
+# Query and print first 10 rows
+query_result <- dbGetQuery(con, "SELECT * FROM fit_data LIMIT 10")
 print(query_result)
 
 # Close the database connection
